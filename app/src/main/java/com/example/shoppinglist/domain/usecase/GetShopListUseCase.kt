0b58e1f80027fedf73dd2b4154f07166f6f06365
Bivis <lumenfan63@mail.ru>
2023-12-28 22:34:27 +0400

    создал viewModel и методы для работы со списком

--- app/src/main/java/com/example/shoppinglist/domain/usecase/GetShopListUseCase.kt
+++ app/src/main/java/com/example/shoppinglist/domain/usecase/GetShopListUseCase.kt
@@ -3,7 +3,7 @@ package com.example.shoppinglist.domain.usecase
 import com.example.shoppinglist.domain.model.ShopItem
 import com.example.shoppinglist.domain.repository.ShopListRepository
 
-class GetIListUseCase(private val shopListRepository: ShopListRepository) {
+class GetShopListUseCase(private val shopListRepository: ShopListRepository) {
     fun getShopList():List<ShopItem>{
        return shopListRepository.getShopList()
     }
---
+++ app/src/main/java/com/example/shoppinglist/presentation/viewmodel/MainViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.shoppinglist.presentation.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.shoppinglist.data.ShopListRepositoryImpl
+import com.example.shoppinglist.domain.model.ShopItem
+import com.example.shoppinglist.domain.usecase.DeleteShopItemUseCase
+import com.example.shoppinglist.domain.usecase.EditShopItemUseCase
+import com.example.shoppinglist.domain.usecase.GetShopListUseCase
+
+class MainViewModel:ViewModel() {
+
+    //временное решение до подключения дагера
+    private val repository = ShopListRepositoryImpl()
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase =EditShopItemUseCase(repository)
+
+    val shopList = MutableLiveData<List<ShopItem>>()
+
+    fun getShopList (){
+        val list = getShopListUseCase.getShopList()
+        shopList.value = list
+    }
+
+    fun deleteShopItem (shopItem: ShopItem){
+        deleteShopItemUseCase.deleteItem(shopItem)
+        getShopList()
+    }
+
+    fun editShopItem (shopItem: ShopItem){
+        val newItem = shopItem.copy(enable = !shopItem.enable)
+        editShopItemUseCase.editShopItem(newItem)
+        getShopList()
+    }
+}
